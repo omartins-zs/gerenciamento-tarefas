03668a82489097080e3b81b030b962172ea9d5c2
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-17 14:14:29 -0300

    Criando Rota de Login, Logout e ShowLoginForm

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+});
+
+Route::post('logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
