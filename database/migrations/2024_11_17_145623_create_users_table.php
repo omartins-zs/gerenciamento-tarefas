e8ce4fd8f3b4a9d944871eb74bbfdf61d7632b6b
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-17 11:57:22 -0300

    Ajustando a Estrutura de colunas da Tabela Users

--- database/migrations/2024_11_17_145623_create_users_table.php
+++ database/migrations/2024_11_17_145623_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
     }
