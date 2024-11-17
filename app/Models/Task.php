bfc340316da4caeb5fbf9d95ee84c6b703ec8810
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-17 12:52:44 -0300

    Ajuste no Fillable e Relação das Task com User

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    /**
+     * Os atributos que podem ser preenchidos em massa.
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+    ];
+
+    /**
+     * Relacionamento com o usuário (muitos para um).
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
