91ca452edbce3470355d3f98da7fdf51c3b9d973
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-17 14:40:34 -0300

    Criando Function para Mostrar Form de Registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     //
+    /**
+     * Exibe o formulário de registro.
+     */
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
 }
