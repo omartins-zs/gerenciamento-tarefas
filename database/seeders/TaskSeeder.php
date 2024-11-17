3a29a068ac6118e5b920c2b90184c8df79714f64
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-17 13:37:28 -0300

    Ajuste no User Seeder e Task Seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,11 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Cria 5 tarefas para cada usuário existente
+        User::all()->each(function ($user) {
+            Task::factory(5)->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,17 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        // Criar um usuário administrador
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        // Criar usuários comuns
+        User::factory(10)->create([
+            'role' => 'user',
+        ]);
     }
 }
