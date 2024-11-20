25d3b4dea7441b35a51e0e6d1d556a1037a2d23b
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 11:52:39 -0300

    Add o Vue no Projeto para as Views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+const app = createApp(App);
+app.mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Bem-vindo ao Vue.js no Laravel!</h1>
+        <p>Este é um exemplo de integração do Vue.js no seu projeto Laravel.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
